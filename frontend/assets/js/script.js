3fde859e334b5fda4ff27e7e2bf2bdf9ef8d5f56
Sara Romero <sara.romerop256@gmail.com>
2026-05-12 20:17:48 +0200

    hecho docker backend + bd + login + registter

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    // =========================
+    // REGISTRO
+    // =========================
     const registerForm = document.getElementById("registerForm");
 
     if (registerForm) {
@@ -18,8 +21,52 @@ document.addEventListener("DOMContentLoaded", () => {
             });
 
             const data = await response.json();
-            console.log(data);
+            console.log("REGISTER:", data);
         });
     }
 
+    // =========================
+    // LOGIN
+    // =========================
+    const loginForm = document.getElementById("loginForm");
+
+    if (loginForm) {
+        loginForm.addEventListener("submit", async (e) => {
+            e.preventDefault();
+
+            const usuario = document.getElementById("usuario").value;
+            const password = document.getElementById("password").value;
+
+            const response = await fetch("http://localhost:3000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ usuario, password })
+            });
+
+            const data = await response.json();
+            console.log("LOGIN:", data);
+
+            if (response.ok) {
+                alert("Login correcto");
+
+                // Guardar sesión
+                localStorage.setItem("usuario", usuario);
+
+                // Redirigir a libro
+                window.location.href = "libro.html";
+            } else {
+                alert("Usuario o contraseña incorrectos");
+            }
+        });
+    }
+    const logoutBtn = document.getElementById("logoutBtn");
+
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", () => {
+            localStorage.removeItem("usuario");
+            window.location.href = "login.html";
+        });
+    }
 });
